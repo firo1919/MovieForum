d679c2a4d612f87a27f7615a9e97d0cfee9dcd7c
firomsa <firomsassf@gmail.com>
2024-08-13 20:46:24 +0300

    initializing

---
+++ src/main/java/com/firomsa/movieforum/MovieforumApplication.java
@@ -0,0 +1,13 @@
+package com.firomsa.movieforum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieforumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieforumApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/firomsa/movieforum/MovieforumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.firomsa.movieforum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieforumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
